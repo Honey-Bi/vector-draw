08bac850d34408edffbe37106417566648ad7e87
Honey-Bi <biten10@naver.com>
2023-08-21 17:51:45 +0900

    top-menu 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return <div className="App">Hello World</div>;
+export default function App() {
+  return (
+    <>
+      <div className="top">
+        <ul className="top-menu">
+          <li>
+            logo
+            <ul className="top-sub-menu">
+              <li>About</li>
+              <li>Keyboard Shortcut</li>
+              <li></li>
+            </ul>
+          </li>
+          <li>
+            File
+            <ul className="top-sub-menu">
+              <li>New Document</li>
+              <li>Export as PNG</li>
+              <li></li>
+            </ul>
+          </li>
+          <li>
+            Edit
+            <ul className="top-sub-menu">
+              <li>Undo</li>
+              <li>Redo</li>
+              <li>Cut</li>
+              <li>Copy</li>
+              <li>Paste</li>
+              <li>Duplicate</li>
+              <li>Delete</li>
+            </ul>
+          </li>
+          <li>
+            Object
+            <ul className="top-sub-menu">
+              <li>Bring to Front</li>
+              <li>Bring Foward</li>
+              <li>Send Backward</li>
+              <li>Send to Back</li>
+              <li>Group Element</li>
+              <li>Ungroup Elements</li>
+            </ul>
+          </li>
+          <li>
+            view
+            <ul className="top-sub-menu">
+              <li>View Rulers</li>
+              <li>View Wierframes</li>
+              <li>Source</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div className="left"></div>
+      <div className="right"></div>
+      <div className="bottom"></div>
+    </>
+  );
 }
-
-export default App;
