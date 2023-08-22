ba2cc6b1c1bf5911b6139c3fb31dad23270d9aac
Honey-Bi <biten10@naver.com>
2023-08-22 21:50:13 +0900

    전역 상태관리 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import LowComponent from "./components/LowComponent";
 import Menu from "./components/menu";
 import Panel from "./components/panel";
 import Tool from "./components/tool";
@@ -12,65 +13,15 @@ export default function App() {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   });
 
+  const handleDataChange = (newData: String) => {
+    console.log(newData);
+  };
+
   return (
-    <>
-      <div className="top">
-        <ul className="top-menu">
-          <li>
-            logo
-            <ul className="top-sub-menu">
-              <li>About</li>
-              <li>Keyboard Shortcut</li>
-              <li></li>
-            </ul>
-          </li>
-          <li>
-            File
-            <ul className="top-sub-menu">
-              <li>New Document</li>
-              <li>Export as PNG</li>
-              <li></li>
-            </ul>
-          </li>
-          <li>
-            Edit
-            <ul className="top-sub-menu">
-              <li>Undo</li>
-              <li>Redo</li>
-              <li>Cut</li>
-              <li>Copy</li>
-              <li>Paste</li>
-              <li>Duplicate</li>
-              <li>Delete</li>
-            </ul>
-          </li>
-          <li>
-            Object
-            <ul className="top-sub-menu">
-              <li>Bring to Front</li>
-              <li>Bring Foward</li>
-              <li>Send Backward</li>
-              <li>Send to Back</li>
-              <li>Group Element</li>
-              <li>Ungroup Elements</li>
-            </ul>
-          </li>
-          <li>
-            view
-            <ul className="top-sub-menu">
-              <li>View Rulers</li>
-              <li>View Wierframes</li>
-              <li>Source</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div className="left"></div>
-      <div className="right"></div>
-      <div className="bottom"></div>
-    </>
+    <GlobalProvider>
       <Menu />
       <Tool />
       <Panel />
+    </GlobalProvider>
   );
 }
