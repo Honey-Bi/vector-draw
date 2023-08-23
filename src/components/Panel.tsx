61b6c463405e6619714ed9f52d939540d0943abd
Honey-Bi <biten10@naver.com>
2023-08-23 17:50:48 +0900

    패널 생성기능 추가중

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -5,5 +5,25 @@ type Props = {
   select: Select;
 };
 function Panel({ tool, select }: Props) {
+  function renderPanel() {
+    let result = [];
+    switch (select) {
+      case null:
+        result.push(
+          <div className="title" key={select}>
+            Canvas Info
+          </div>
+        );
+        break;
+    }
+    result.push(
+      <div className="" key={tool}>
+        <div className=""></div>
+      </div>
+    );
+    return result;
+  }
+  return <div className="panel">{renderPanel()}</div>;
 }
+
 export default Panel;
