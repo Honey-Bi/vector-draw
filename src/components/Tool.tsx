5b637dba97f634c05743ce1a9f6e7a6b0cdce116
Honey-Bi <biten10@naver.com>
2023-08-23 17:51:02 +0900

    Update Tool.tsx

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -159,45 +159,9 @@ function Tool({ tool, setTool, palette, setPalette }: Props) {
           <div className="color-default" onClick={colorDefault}></div>
         </div>
       </div>
-      <div
-        className={`tool-button ${tool === "path" ? "active" : ""}`}
-        title="Path Tool"
-        onClick={(e) => setTool("path")}
-      >
-        <svg xmlns={xmlns} viewBox="0 0 27 27">
-          <path d="M12.2 1.9c0-.36.86 0 .86 0V14a1.3 1.3 0 10.88 0V1.9s.87-.36.87 0c0 6.81 5.22 11.68 5.22 11.68l-3.25 8.2h-6.55l-3.26-8.2s5.22-4.87 5.22-11.68zM7.83 25.26v-2.61h11.32v2.6H7.84z"></path>
-        </svg>
-      </div>
-      <div
-        className={`tool-button ${tool === "text" ? "active" : ""}`}
-        title="Text Tool"
-        onClick={(e) => setTool("text")}
-      >
-        <svg xmlns={xmlns} viewBox="2 2 20 20">
-          <path d="M5 4v3h5.5v12h3V7H19V4z"></path>
-        </svg>
-      </div>
-      <div
-        className={`tool-button ${tool === "zoom" ? "active" : ""}`}
-        title="Zoom Tool"
-        onClick={(e) => setTool("zoom")}
-      >
-        <svg xmlns={xmlns} viewBox="2 2 20 20">
-          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-        </svg>
-      </div>
-      <div
-        className={`tool-button mirror ${tool === "spoid" ? "active" : ""}`}
-        title="Spoid Tool"
-        onClick={(e) => setTool("spoid")}
-      >
-        <svg xmlns={xmlns} viewBox="2 2 20 20">
-          <path d="M20.71 5.63l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-3.12 3.12-1.93-1.91-1.41 1.41 1.42 1.42L3 16.25V21h4.75l8.92-8.92 1.42 1.42 1.41-1.41-1.92-1.92 3.12-3.12c.4-.4.4-1.03.01-1.42zM6.92 19L5 17.08l8.06-8.06 1.92 1.92L6.92 19z"></path>
-        </svg>
-      </div>
-      <div className="color-tool"></div>
-    </div>
+      <div className="color-picker"></div>
+    </>
   );
-};
+}
 
 export default Tool;
