4c48896709780da054449022f6f20925bc141f84
Honey-Bi <biten10@naver.com>
2023-08-23 12:47:55 +0900

    vh 크기조절 기능 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import Tool from "./components/tool";
 import "./index.css";
 
 export default function App() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-  window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
 
 
   return (
