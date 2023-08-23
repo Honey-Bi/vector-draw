1dbfbbc15acd3dbb8fa2f551161edd4e22172744
Honey-Bi <biten10@naver.com>
2023-08-23 12:47:30 +0900

    전역 상태 관리 연습 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import LowComponent from "./components/LowComponent";
 import Menu from "./components/menu";
 import Panel from "./components/panel";
 import Tool from "./components/tool";
@@ -13,15 +12,8 @@ export default function App() {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   });
 
-  const handleDataChange = (newData: String) => {
-    console.log(newData);
-  };
 
   return (
-    <GlobalProvider>
       <Menu />
-      <Tool />
-      <Panel />
-    </GlobalProvider>
   );
 }
