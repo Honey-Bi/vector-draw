67aa98e73440938484dc8e8aee607bd393ed97f9
Honey-Bi <biten10@naver.com>
2023-08-24 17:52:44 +0900

    선택자 정보읽기 기능 추가중

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,26 +1,81 @@
-import { Select, Tools } from "../types";
+import React from "react";
+import { Select, Size, SvgObject, Tools } from "../types";
 
 type Props = {
   tool: Tools;
   select: Select;
+  canvasSize: Size;
+  setCanvasSize: (e: Size) => void;
 };
-function Panel({ tool, select }: Props) {
+function Panel({ tool, select, canvasSize, setCanvasSize }: Props) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.id === "height") {
+      setCanvasSize({
+        width: canvasSize.width,
+        height: Number(e.target.value),
+      });
+    } else if (e.target.id === "width") {
+      setCanvasSize({
+        width: Number(e.target.value),
+        height: canvasSize.height,
+      });
+    }
+  }
   function renderPanel() {
     let result = [];
-    switch (select) {
-      case null:
-        result.push(
-          <div className="title" key={select}>
-            Canvas Info
+    if (select) {
+      switch (select.tagName) {
+        case "rect":
+          <div key={select.id}></div>;
+          break;
+        case "line":
+          result.push(
+            <div key={select.id}>
+              <div className="select">Line</div>
+            </div>
+          );
+          break;
+      }
+    } else {
+      result.push(
+        <div key={select}>
+          <div className="select">Canvas</div>
+          <div className="input-group">
+            <label htmlFor="title">title</label>
+            <input
+              className="panel-input"
+              type="text"
+              id="title"
+              placeholder="NoTitle"
+            />
           </div>
-        );
-        break;
+          <div className="row">
+            <div className="input-group">
+              <label htmlFor="width">width</label>
+              <input
+                type="number"
+                id="width"
+                min="1"
+                onChange={handleChange}
+                value={canvasSize.width}
+                className="panel-input"
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="height">height</label>
+              <input
+                type="number"
+                id="height"
+                min="1"
+                className="panel-input"
+                onChange={handleChange}
+                value={canvasSize.height}
+              />
+            </div>
+          </div>
+        </div>
+      );
     }
-    result.push(
-      <div className="" key={tool}>
-        <div className=""></div>
-      </div>
-    );
     return result;
   }
   return <div className="panel">{renderPanel()}</div>;
