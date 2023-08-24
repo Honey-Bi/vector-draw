7fc539e6d28e1657d985831d43782e7ce9664262
Honey-Bi <biten10@naver.com>
2023-08-24 17:53:01 +0900

    타입 통합

--- src/types.tsx
+++ src/types.tsx
@@ -11,8 +11,9 @@ type Tools =
   | "spoid";
 
 type Position = { x: number; y: number };
+type Size = { width: number; height: number };
 
-type Select = SVGElement | null;
+type Select = SVGSVGElement | null;
 
 type Color = { r: number; g: number; b: number } | null;
 
@@ -20,4 +21,26 @@ type Palette = { fill: Color; stroke: Color };
 
 type KeyBind = { ctrl: boolean; shift: boolean; alt: boolean };
 
-export type { Tools, Position, Select, Palette, KeyBind };
+type SvgType =
+  | "polyline"
+  | "line"
+  | "rect"
+  | "Ellipse"
+  | "Polygon"
+  | "path"
+  | "text";
+
+type SvgObject = {
+  title: string;
+  type: SvgType;
+  position?: Position;
+  position1?: Position;
+  position2?: Position;
+  fill?: Color;
+  size?: Size;
+  stroke?: Color;
+  strokeWidth?: number;
+  points?: Position[];
+};
+
+export type { Tools, Position, Select, Palette, KeyBind, SvgObject, Size };
